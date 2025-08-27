0f9d7a5b615dc6d16b6fb814811db19fe5921b1f
Gali Johnson Rohith Reddy <grohithreddy2003@gmail.com>
2025-08-27 20:04:02 +0530

    intial commit

--- migrations/V1.1.1__initial_objects.sql
+++
@@ -1,6 +0,0 @@
-CREATE SCHEMA {{ schema_name }};
-CREATE TABLE {{ database_name }}.{{ schema_name }}.HELLO_WORLD_test
-(
-   FIRST_NAME VARCHAR
-  ,LAST_NAME VARCHAR
-);
--- migrations/V1.1.2__initial_objects.sql
+++
@@ -1,9 +0,0 @@
--- Use idempotent syntax to avoid errors if schema exists
-CREATE SCHEMA IF NOT EXISTS {{ schema_name }};
-
--- Create or replace table to ensure it’s in the right schema
-CREATE OR REPLACE TABLE {{ database_name }}.{{ schema_name }}.HELLO_WORLD_test_22
-(
-   FIRST_NAME VARCHAR
-  ,LAST_NAME VARCHAR
-);
